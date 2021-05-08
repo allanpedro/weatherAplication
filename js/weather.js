9f05b34d02f9123c84cdf3ab7e82ef36abf3b1df
allanpedro <allan.caldi@outlook.com>
2021-05-08 13:01:05 -0300

    weather

---
+++ js/weather.js
@@ -0,0 +1,32 @@
+const baseUrl = 'https://dataservice.accuweather.com/'
+const APIKey = 'pIxhE1xHyIR0ZVZvAOi86bAR2MMbrdUG'
+
+const getCityURL= cityName => `${baseUrl}locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getWeatherUrl= cityKey => `${baseUrl}currentconditions/v1/${cityKey}?apikey=${APIKey}&language=pt-br`
+
+const fetchData = async url => {
+    try {
+        const response = await fetch(url)
+
+        if (!response.ok) {
+            throw new Error('Não foi possível obter os dados!')
+        }
+
+        return response.json()
+    } catch ({ name, message}) {
+        alert(`${name}: ${message}`)
+    }
+}
+
+const getCityName = cityName => {
+    const cityUrl = getCityURL(cityName)
+    return fetchData(cityUrl)
+}
+
+const getCityWeather = key => {
+    const weatherUrl = getWeatherUrl(key)
+    return fetchData(weatherUrl)
+}
+
+getCityWeather('Curitiba')
